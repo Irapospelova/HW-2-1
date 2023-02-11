e4772d7b408b4221cefa7433cc0e5f6a1f8d75ed
Irina Pospelova <irapospelova78@gmail.com>
2023-02-11 19:00:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 18_350;
+        int mile = 20;
+        int bonus = ticketPrice / mile;
+        System.out.println("Начислено бонусов:" + bonus);
+
+    }
+}
